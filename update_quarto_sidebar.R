6320afd305cb774f1a33cee6b31dca6995d468df
iramler <iramler@users.noreply.github.com>
2025-05-28 10:36:38 -0400

    Add GitHub Actions auto-deploy for Quarto site

---
+++ update_quarto_sidebar.R
@@ -0,0 +1,27 @@
+# update_quarto_sidebar.R
+library(yaml)
+
+# Read the existing _quarto.yml
+qfile <- "_quarto.yml"
+q <- read_yaml(qfile)
+
+# Detect folders with index.qmd (excluding hidden/system ones)
+folders <- list.dirs(".", recursive = FALSE, full.names = FALSE)
+folders <- folders[file.exists(file.path(folders, "index.qmd")) & !grepl("^_", folders)]
+
+# Update sidebar with About + Modules by Sport
+q$website$sidebar <- list(
+  contents = list(
+    list(
+      section = "About",
+      contents = c("_team.qmd", "by-statsds-topic.qmd")
+    ),
+    list(
+      section = "Modules by Sport",
+      contents = list(list(auto = TRUE, path = "."))
+    )
+  )
+)
+
+# Write updated YAML
+writeLines(as.yaml(q), qfile)
