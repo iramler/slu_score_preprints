48520ca41120f4e9dc25dd4ebadf52fc54546e21
iramler <iramler@users.noreply.github.com>
2025-05-28 11:02:32 -0400

    Use safe sidebar rewrite to avoid breaking YAML booleans

--- update_quarto_sidebar.R
+++ update_quarto_sidebar.R
@@ -1,34 +1,54 @@
-# update_quarto_sidebar.R
 library(yaml)
 
-# Read the existing _quarto.yml
 qfile <- "_quarto.yml"
-q <- read_yaml(qfile)
 
-# Detect folders with index.qmd (excluding hidden/system ones)
+# Detect folders with index.qmd
 folders <- list.dirs(".", recursive = FALSE, full.names = FALSE)
 folders <- folders[file.exists(file.path(folders, "index.qmd")) & !grepl("^_", folders)]
 
-# Update sidebar with About + Modules by Sport
-q$website$sidebar <- list(
-  contents = list(
-    list(
-      section = "About",
-      contents = c("_team.qmd", "by-statsds-topic.qmd")
-    ),
-    list(
-      section = "Modules by Sport",
-      contents = list(list(auto = TRUE, path = "."))
+# Create updated sidebar YAML block
+new_sidebar <- list(
+  website = list(
+    sidebar = list(
+      contents = list(
+        list(
+          section = "About",
+          contents = c("_team.qmd", "by-statsds-topic.qmd")
+        ),
+        list(
+          section = "Modules by Sport",
+          contents = list(list(auto = TRUE, path = "."))
+        )
+      )
     )
   )
 )
 
-# Write updated YAML
-# Write YAML with correct booleans (true/false without quotes)
-# More robust replacement of quoted booleans
-yaml_text <- as.yaml(q)
-yaml_text <- gsub("\\s*toc: 'true'", "  toc: true", yaml_text)
-yaml_text <- gsub("\\s*toc: 'false'", "  toc: false", yaml_text)
-writeLines(yaml_text, qfile)
+# Dump just the sidebar as YAML
+sidebar_yaml <- as.yaml(new_sidebar$website$sidebar, indent.mapping.sequence = TRUE)
+sidebar_yaml_lines <- paste("  sidebar:", gsub("\n", "\n    ", sidebar_yaml), sep = "\n")
 
+# Read original _quarto.yml lines
+yml_lines <- readLines(qfile)
 
+# Replace the existing sidebar block
+start_idx <- grep("^\\s*sidebar:", yml_lines)
+if (length(start_idx) > 0) {
+  # Remove existing sidebar block
+  end_idx <- start_idx
+  while (end_idx < length(yml_lines) && !grepl("^\\s*[^\\s]", yml_lines[end_idx + 1])) {
+    end_idx <- end_idx + 1
+  }
+  yml_lines <- c(
+    yml_lines[1:(start_idx - 1)],
+    sidebar_yaml_lines,
+    yml_lines[(end_idx + 1):length(yml_lines)]
+  )
+} else {
+  # Append if sidebar is not found
+  insert_idx <- grep("^website:", yml_lines)
+  yml_lines <- append(yml_lines, sidebar_yaml_lines, after = insert_idx)
+}
+
+# Write back the updated file
+writeLines(yml_lines, qfile)
